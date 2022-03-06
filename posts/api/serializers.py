25a107ccc536281a7ae75c274b6be9c0b46be37e
Ishaan Sharma <ishaan.sharma@ezeiatech.in>
2022-03-06 12:43:21 +0530

    comment serializer added

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from posts.models import Post
+from posts.models import Post, Comment
 
 class PostSerializer(serializers.ModelSerializer):
     post_author = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Post
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+    original_post = serializers.StringRelatedField(read_only=True)
+    comment_author = serializers.StringRelatedField(read_only=True)
+    class Meta:
+        model = Comment
         fields = '__all__'
--- posts/models.py
+++ posts/models.py
@@ -6,3 +6,9 @@ class Post(models.Model):
     upload_time = models.DateTimeField(auto_now_add=True)
     post_text = models.TextField()
     post_image = models.ImageField(upload_to='post_images/')
+
+class Comment(models.Model):
+    original_post = models.ForeignKey("posts.Post", on_delete=models.CASCADE)
+    comment_author = models.ForeignKey("accounts.AllUsers", on_delete=models.CASCADE)
+    upload_time = models.DateTimeField(auto_now_add=True)
+    comment_text = models.TextField()
